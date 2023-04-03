a08fca09d0852f86314ea2a6e812c4ccb43c1cb6
atomeV8 <94994373+atomeV8@users.noreply.github.com>
2023-04-03 16:29:10 +0200

    Initial commit

---
+++ src/com/atomev8/component/MenuButton.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atomev8.component;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author CP-20MAB
+ */
+public class MenuButton extends JButton{
+    public MenuButton(){
+        setContentAreaFilled(false);
+    }
+}
